203943165646af51ee641b236aa19ffe29c20444
Gina Graci <ginagraci92@gmail.com>
2019-11-16 14:31:29 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,30 @@
+class Genre 
+  
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def Genre.destroy_all
+    @@all.clear
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=artist if artist
+    self.genre=genre if genre
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def Song.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+    # Artist.all.push(artist)
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    if !(genre.songs.include?(self))
+      genre.songs << self
+      # Genre.all.push(genre)
+    # binding.pry
+    end
+  end
+  
+end 
